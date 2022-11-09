b097fd4ab68b237639962cf20a9198f43d4f3829
MichalMiskiewicz <miskiewiczmd@gmail.com>
2022-11-09 10:36:34 +0100

    Initial commit

---
+++ src/main/java/com/miskiewicz/michal/foodorderingsystem/FoodOrderingSystemApplication.java
@@ -0,0 +1,14 @@
+package com.miskiewicz.michal.foodorderingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FoodOrderingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodOrderingSystemApplication.class, args);
+    }
+
+}
